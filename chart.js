b751a9cb0485bfa50a73b3f59ef7f89de6786eb8
Kevin <kevin@storeplacer.com>
2015-11-05 10:24:01 -0600

    adding second chart

--- chart.js
+++ chart.js
@@ -1,15 +1,34 @@
-var data = {
+var data, oosData;
+
+data = {
   columns: [
     ['data1', 30, 40, 50, 100, 125],
     ['data2', 225, 140, 150, 100, 75]
   ]
 };
 
-function drawChart() {
+oosData = {
+  columns: [
+    ['Overnight', 44.56, 22.98, 9.55, 0, 0],
+    ['Breakfast', 177.32, 140, 150, 100, 75],
+    ['Lunch', 225, 140, 150, 100, 75],
+    ['Rush Hour', 225, 140, 150, 100, 75],
+    ['Evening', 225, 140, 150, 100, 75],
+  ],
+  type: 'bar',
+  groups: [
+    ['Overnight','Breakfast','Lunch','Rush Hour','Evening']
+  ]
+};
+
+function drawChart(domNodeId, data) {
   var chart = c3.generate({
-    bindto: '#chart',
+    bindto: domNodeId,
     data: data
   });
 }
 
-drawChart();
+
+drawChart('#chart1', data);
+
+drawChart('#chart2', oosData);
